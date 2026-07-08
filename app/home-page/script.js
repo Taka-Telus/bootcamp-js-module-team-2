a26a66170319e4aa47526f96845719ee985c92fb
mayragm2 <mayracueto@gm2dev.com>
2026-07-08 12:56:45 -0300

    feat(home-page): add inner html to show question count, game difficulty and individual game ids

--- app/home-page/script.js
+++ app/home-page/script.js
@@ -1,35 +1,49 @@
-const gamesDisplayBar = document.getElementById("games-menu-home");
-const apiURL = "https://quiz-api.cesar-kastli.workers.dev/games"
 async function traerUsuario() {
-  try {
-    const respuesta = await fetch(apiURL);
-    const datos = await respuesta.json();
-    //console.log(JSON.stringify(datos));
-    localStorage.setItem("gameData", JSON.stringify(datos));
-} catch (error) {
-    console.log("Falló:", error);
+    try {
+        const respuesta = await fetch(apiURL);
+        const datos = await respuesta.json();
+        //console.log(JSON.stringify(datos));
+        localStorage.setItem("gameData", JSON.stringify(datos));
+    } catch (error) {
+        console.log("Falló:", error);
+    }
 }
-}
-setTimeout(traerUsuario());
-const data = localStorage.getItem("gameData");
-const gameData = JSON.parse(data);
-console.log (data);
-gameData.forEach(game => {
-    const gameCard = document.createElement("div");
-    gameCard.classList.add("card");
-    console.log (game);
-    gameCard.innerHTML = `
-    <img src="${game.image}">
-    <div class="game-card-info">
-                <h4 id="game1-name">Game Name</h4>
-                <h6>${game.questionCount} questions</h6>
-                <h6>${game.difficulty}</h6>
-            </div>
-            <div>
-                <button id="play-button-game-1">PLAY</button>
-                <button id="edit-button-game-1">✎</button>
-            </div>
-    `
 
-    gamesDisplayBar.appendChild(gameCard);
-});
+;(async () => {
+
+    const gamesDisplayBar = document.getElementById("games-menu-home");
+    const apiURL = "https://quiz-api.cesar-kastli.workers.dev/games"
+    await traerUsuario();
+    const data = localStorage.getItem("gameData");
+    const gameData = JSON.parse(data);
+    console.log (data);
+    gameData.forEach(game => {
+        const gameCard = document.createElement("div");
+        gameCard.classList.add("card");
+        console.log (game);
+        gameCard.innerHTML = `
+        <img src="${game.image}">
+        <div class="game-card-info">
+                    <h4 id="game1-name">Game Name</h4>
+                    <h6>${game.questionCount} questions</h6>
+                    <h6>${game.difficulty}</h6>
+                </div>
+                <div>
+                    <button id="play-${game.id}">PLAY</button>
+                    <button id="edit-${game.id}">✎</button>
+                </div>
+        `
+        gamesDisplayBar.appendChild(gameCard);
+    });
+
+    const buttons = document.getElementsByTagName("button");
+    Array.from(buttons).forEach(button => { 
+    button.addEventListener ("click", () => {
+    
+
+    });
+    })
+    
+})();
+
+
