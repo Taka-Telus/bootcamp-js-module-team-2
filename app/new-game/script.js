06ae69a054ad154dfd45a342dfd0a848db846ea9
mayragm2 <mayracueto@gm2dev.com>
2026-07-15 12:41:10 -0300

    feat(new-game): create new game page layout and functionality for adding questions

---
+++ app/new-game/script.js
@@ -0,0 +1,24 @@
+const questionDisplay = document.getElementById("questions");
+const addQuestionButton = document.getElementById ("add-questions");
+const apiURL = `https://quiz-api.cesar-kastli.workers.dev/games/`
+
+
+addQuestionButton.addEventListener("click", addQuestionHandler);
+
+function addQuestionHandler () {
+    const newQuestion = document.createElement ("div");
+    newQuestion.classList.add("card");
+    newQuestion.innerHTML = `
+    <div class="header">
+    <input id="question" placeholder = "type your question here"></input>
+    <button>🗑️</button>
+    </div>
+    <div>
+    <input id="answer1" class = "right-answer" placeholder = "type your answer here"></input>
+    <input id="answer2" placeholder = "type your answer here"></input>
+    <input id="answer3" placeholder = "type your answer here"></input>
+    <input id="answer4" placeholder = "type your answer here"></input>
+    </div>
+    `
+    questionDisplay.appendChild (newQuestion);
+}
