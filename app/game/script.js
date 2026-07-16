5e8c0e1688533c26224e53ffea6114463bfe53b3
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-07-16 12:54:59 -0300

    fix(script): remove unnecessary whitespace in traerJuego function

--- app/game/script.js
+++ app/game/script.js
@@ -16,6 +16,7 @@ async function traerJuego(apiURL) {
         localStorage.setItem("gameData", JSON.stringify(datos));
     } catch (error) {
         console.log("Falló:", error);
+        
     }
 }
 
