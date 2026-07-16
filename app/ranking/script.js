d9aa5d97d7628099dc5249064c8a6b595ab38ff7
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-07-16 12:13:12 -0300

    fix(sound): improve sound playback logic in ranking script

--- app/ranking/script.js
+++ app/ranking/script.js
@@ -2,17 +2,31 @@ const scoreDisplay = document.getElementById("final-score-ranking");
 const ansRigthDisp = document.getElementById("right-anwers-ranking");
 const puntos = localStorage.getItem("puntos");
 
+const soundEnabled = localStorage.getItem("sound") === "true";
+const farfareSound = new Audio("../ranking/farfare.mp3");
+
 confetti({
   particleCount: 250,
   spread: 360,
   origin: { y: 0.5 },
   colors: [
-      "#007b06",
-      "#bea100",
-      "#002592",
-      "#d10000"
+    "#007b06",
+    "#bea100",
+    "#002592",
+    "#d10000"
   ]
 });
 
-scoreDisplay.innerHTML = `${puntos*100}`;
-ansRigthDisp.innerHTML = `${puntos} Respuestas Correctas`;
+scoreDisplay.innerHTML = `${puntos * 100}`;
+ansRigthDisp.innerHTML = `${puntos} Respuestas Correctas`; {
+  
+  if (soundEnabled) {
+    farfareSound.play();
+  }
+  else (soundEnabled === false); {
+
+    farfareSound.pause();
+    
+} 
+}
+//no funciona el sonido, no se reproduce, no se si es por el navegador o por el codigo, pero no se reproduce, review
